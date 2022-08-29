7dd654d1ee83f6ca756f2ba760246bfa6111440b
Zachattack221 <zsvd09@gmail.com>
2022-08-29 00:41:18 -0400

    working on formatting document, setting up <div> and <p> elements to populate later with API information

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,25 @@
 $('.container').on('click','.submitBtn', function() {
     var cityEl = $(this).prev().val();
-    localStorage.setItem('button', cityEl);
-    var lastSearch = document.createElement('button');
+    localStorage.setItem('cityname', cityEl);
+    var lastSearch = document.createElement('li');
     lastSearch.textContent = cityEl;
-    document.body.appendChild(lastSearch);
+    console.log(cityEl);
+    $('#previousSearchSection').append(JSON.stringify(cityEl).toUpperCase());
+    $('.allSearches').addClass('btn-primary btn-lg btn-block');
+
+
+
+    // if (p !== null) {
+
+    // }
+    // console.log(typeof(lastSearch));
+    // console.log(JSON.stringify(lastSearch));
 });
+
+// $('#previousSearchSection').on('click','')
+
+// TODO: button element with previous search needs to be functionally connected to search bar with function
+
+// TODO: create functions that accociate with API key for open weather map
+
+// TODO: remember to convert units to imperial 
